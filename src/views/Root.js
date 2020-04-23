744236934428899d81bb8ed4d459c34e3278327d
dadu109 <heniek12322@gmail.com>
2020-04-23 13:25:54 +0200

    initial commit

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App() {
+function Root() {
   return (
     <div>
       siema
@@ -8,4 +8,4 @@ function App() {
   );
 }
 
-export default App;
+export default Root;
