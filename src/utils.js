c69d20ec3231db802e5781183ece3d0a78e5d6d9
dadu109 <heniek12322@gmail.com>
2020-05-01 00:31:24 +0200

    utils change

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,16 @@
-export const monthNames = [null,...Array.from({length: 12}, (x, i) => {
+export const monthNames = [null, ...Array.from({length: 12}, (x, i) => {
     const str = new Date(0, i + 1, 0).toLocaleDateString('pl-PL', {month: 'long'});
     return str.charAt(0).toUpperCase() + str.slice(1)
 })];
 
-export const years = [{name:2020},{name:2021},{name:2022}];
+export const years = [
+    {name: 2020},
+    {name: 2021},
+    {name: 2022},
+    {name: 2023},
+    {name: 2024},
+    {name: 2025}
+];
 
 export const randCol = [
     {
@@ -33,39 +40,39 @@ export const randCol = [
 
 export const assignments = [
     {
-        title:'Rozprawka',
-        subject:'Język Polski',
-        dueDate:new Date(2020,5,3,10,10),
-        desc:'Rozprawka ma być o czymś tam związanym z językiem polskim sample text ja tu tylko przedłużam '
+        title: 'Rozprawka',
+        subject: 'Język Polski',
+        dueDate: new Date(2020, 5, 3, 10, 10),
+        desc: 'Rozprawka ma być o czymś tam związanym z językiem polskim sample text ja tu tylko przedłużam '
     },
     {
-        title:'Charakterystyka',
-        subject:'Język Polski',
-        dueDate:new Date(2020,5,12,10,10),
-        desc:'Rozprawka ma być o czymś tam związanym z językiem polskim sample text ja tu tylko przedłużam '
+        title: 'Charakterystyka',
+        subject: 'Język Polski',
+        dueDate: new Date(2020, 5, 12, 10, 10),
+        desc: 'Rozprawka ma być o czymś tam związanym z językiem polskim sample text ja tu tylko przedłużam '
     },
     {
-        title:'Sprawdzian Homograficzna',
-        subject:'Matematyka',
-        dueDate:new Date(2020,4,31,12,),
-        desc:'Rozprawka ma być o czymś tam związanym z językiem polskim sample text ja tu tylko przedłużam '
+        title: 'Sprawdzian Homograficzna',
+        subject: 'Matematyka',
+        dueDate: new Date(2020, 4, 31, 12,),
+        desc: 'Rozprawka ma być o czymś tam związanym z językiem polskim sample text ja tu tylko przedłużam '
     },
     {
-        title:'System UI',
-        subject:'Fizyka',
-        dueDate:new Date(2020,5,21,15,20),
-        desc:'Rozprawka ma być o czymś tam związanym z językiem polskim sample text ja tu tylko przedłużam '
+        title: 'System UI',
+        subject: 'Fizyka',
+        dueDate: new Date(2020, 5, 21, 15, 20),
+        desc: 'Rozprawka ma być o czymś tam związanym z językiem polskim sample text ja tu tylko przedłużam '
     },
     {
-        title:'Rozprawka',
-        subject:'Język Polski',
-        dueDate:new Date(2020,5,3,10,10),
-        desc:'Rozprawka ma być o czymś tam związanym z językiem polskim sample text ja tu tylko przedłużam '
+        title: 'Rozprawka',
+        subject: 'Język Polski',
+        dueDate: new Date(2020, 5, 3, 10, 10),
+        desc: 'Rozprawka ma być o czymś tam związanym z językiem polskim sample text ja tu tylko przedłużam '
     },
     {
-        title:'Coś z wfu',
-        subject:'WF',
-        dueDate:new Date(2020,5,3,10,10),
-        desc:'Rozprawka ma być o czymś tam związanym z językiem polskim sample text ja tu tylko przedłużam '
+        title: 'Coś z wfu',
+        subject: 'WF',
+        dueDate: new Date(2020, 5, 3, 10, 10),
+        desc: 'Rozprawka ma być o czymś tam związanym z językiem polskim sample text ja tu tylko przedłużam '
     }
 ];
